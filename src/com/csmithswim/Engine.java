5bdfa37d67b376cb3053525eec33563d09f88814
Cullen S <cullensmi@gmail.com>
2021-08-28 15:03:56 -0400

    first commit

---
+++ src/com/csmithswim/Engine.java
@@ -0,0 +1,10 @@
+package com.csmithswim;
+
+class Engine {
+    public String type;
+
+    Engine(String type) {
+        this.type = type;
+    }
+
+}
---
+++ src/com/csmithswim/Vehicle.java
@@ -0,0 +1,13 @@
+package com.csmithswim;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Vehicle {
+    public Engine engine;
+
+    Vehicle(Engine engine){
+        this.engine = engine;
+    }
+
+}
